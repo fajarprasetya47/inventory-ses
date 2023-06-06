245bf20d02fb23b6b812a7807dcb9bcc030905fe
Fajar Prasetya <japra1701@gmail.com>
2023-06-06 20:20:31 +0700

    fix barangkeluar, barangmasuk, and pengguna

--- app/routes/barangmasuk.jsx
+++ app/routes/barangmasuk.jsx
@@ -1,10 +1,43 @@
 import Dashboard from "../components/Dashboard/Dashboard";
+import Tabel from "../components/Tabel";
 
 export default function BarangMasuk() {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  ];
   return (
     <>
       <Dashboard title="Transaksi Barang Masuk" active='transaksi'>
-        Hallo Transaksi Barang Masuk
+        <div className="my-2">
+          <button className="btn btn-sm btn-success">+ Barang Masuk</button>
+        </div>
+        <Tabel columns={columns} rows={rows} />
       </Dashboard>
     </>
   );
