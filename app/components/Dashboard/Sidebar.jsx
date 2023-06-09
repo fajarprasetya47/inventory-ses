bc4d689e89b01dab84fb0338c6d147752ab9dd28
Fajar Prasetya <japra1701@gmail.com>
2023-06-09 22:56:58 +0700

    fix sidebar and home style

--- app/components/Dashboard/Sidebar.jsx
+++ app/components/Dashboard/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Link } from "@remix-run/react";
+
 export default function Sidebar({ active }) {
     const [display, setDisplay] = useState(false);
     const handleOpen = () => {
@@ -17,22 +19,22 @@ export default function Sidebar({ active }) {
                 <div className="my-2 text-center">
                     <img src='images/logo3.svg' alt="logo" />
                 </div>
-                <div className={active == 'home' ? "sidebar-link-active" : "sidebar-link"}>
-                    <a href="/">
+                <Link to='/'>
+                    <div className={active == 'home' ? "sidebar-link-active" : "sidebar-link"}>
                         <div className="sidebar-link-icon">
                             <img src='images/house-door.svg' alt="house-door" />
                         </div>
                         Home
-                    </a>
-                </div>
-                <div className={active == 'barang' ? "sidebar-link-active" : "sidebar-link"}>
-                    <a href="/barang">
+                    </div>
+                </Link>
+                <Link to='/barang'>
+                    <div className={active == 'barang' ? "sidebar-link-active" : "sidebar-link"}>
                         <div className="sidebar-link-icon">
                             <img src='images/database.svg' alt="database" />
                         </div>
                         Barang
-                    </a>
-                </div>
+                    </div>
+                </Link>
                 <div onClick={handleOpen} className={active == 'transaksi' ? "sidebar-link-active" : "sidebar-link"}>
                     <div className="sidebar-link-icon">
                         <img src='images/arrow-left-right.svg' alt="arrow-left-right" />
@@ -42,26 +44,26 @@ export default function Sidebar({ active }) {
                 </div>
                 <div
                     className="border-bottom"
-                    style={{ 
+                    style={{
                         display: `${display == true ? 'block' : 'none'}`,
                     }}
                 >
-                    <div className="sidebar-link">
-                        <a href="/barangmasuk">
+                    <Link to='/barangmasuk'>
+                        <div className="sidebar-link">
                             <div>
                                 <img src='images/arrow-down-right-circle.svg' alt="arrow-down-right-circle" />
                             </div>
                             Barang Masuk
-                        </a>
-                    </div>
-                    <div className="sidebar-link">
-                        <a href="/barangkeluar">
+                        </div>
+                    </Link>
+                    <Link to='/barangkeluar'>
+                        <div className="sidebar-link">
                             <div>
                                 <img src='images/arrow-up-left-circle.svg' alt="arrow-up-left-circle" />
                             </div>
                             Barang Keluar
-                        </a>
-                    </div>
+                        </div>
+                    </Link>
                 </div>
                 {/* <div className={active == 'penjualan' ? "sidebar-link-active" : "sidebar-link"}>
                     <a href="/penjualan">
@@ -71,30 +73,30 @@ export default function Sidebar({ active }) {
                         Data Penjualan
                     </a>
                 </div> */}
-                <div className={active == 'laporan' ? "sidebar-link-active" : "sidebar-link"}>
-                    <a href="/laporan">
+                <Link to='/laporan'>
+                    <div className={active == 'laporan' ? "sidebar-link-active" : "sidebar-link"}>
                         <div className="sidebar-link-icon">
                             <img src='images/file-earmark.svg' alt="file-earmark" />
                         </div>
                         Laporan
-                    </a>
-                </div>
-                <div className={active == 'prediksi' ? "sidebar-link-active" : "sidebar-link"}>
-                    <a href="/prediksi">
+                    </div>
+                </Link>
+                <Link to='/prediksi'>
+                    <div className={active == 'prediksi' ? "sidebar-link-active" : "sidebar-link"}>
                         <div className="sidebar-link-icon">
                             <img src='images/graph-up-arrow.svg' alt="graph-up-arrow" />
                         </div>
                         Prediksi
-                    </a>
-                </div>
-                <div className={active == 'pengguna' ? "sidebar-link-active" : "sidebar-link"}>
-                    <a href="/pengguna">
+                    </div>
+                </Link>
+                <Link to='/pengguna'>
+                    <div className={active == 'pengguna' ? "sidebar-link-active" : "sidebar-link"}>
                         <div className="sidebar-link-icon">
                             <img src='images/person.svg' alt="person" />
                         </div>
                         Pengguna
-                    </a>
-                </div>
+                    </div>
+                </Link>
             </div>
         </>
     )
