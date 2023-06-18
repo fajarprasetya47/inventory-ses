f6b75265402ed8fb7c5198b82ec6b5b0514c5d31
yufajjiru-dev <luthfi.yufajjiru@efishery.com>
2023-06-18 11:47:53 +0700

    add mape and mad value in prediksiresult

--- app/components/Prediksi/PrediksiResult.jsx
+++ app/components/Prediksi/PrediksiResult.jsx
@@ -11,6 +11,22 @@ const sesForecast = (data, alpha) => {
   })
   return fore;
 }
+const madValue = (data) => {
+  let y = 0;
+  data?.forEach((item) => {
+    const t = Math.abs(item?.total - item?.prediksi);
+    y += t;
+  })
+  return (y/data?.length).toFixed(3);
+}
+const mapeValue = (data) => {
+  let y = 0;
+  data?.forEach((item) => {
+    const t = Math.abs(((item?.total - item?.prediksi) / item?.total)) * 100;
+    y += t;
+  })
+  return (y/data?.length).toFixed(3);
+}
 
 export default function PrediksiResult() {
   const data = useActionData()?.dataMonthly;
@@ -19,6 +35,8 @@ export default function PrediksiResult() {
   const namaBarang = barang?.find(item => item?.id === idBarang)?.namaBarang;
   const [result, setResult] = useState([]);
   const [alpha, setAlpha] = useState();
+  const [mad, setMad] = useState();
+  const [mape, setMape] = useState();
 
   const columns = [
     {
@@ -43,6 +61,8 @@ export default function PrediksiResult() {
     if (parseFloat(alpha) > 1) return;
     const newForecast = sesForecast(result, alpha);
     setResult(newForecast);
+    setMad(madValue(newForecast));
+    setMape(mapeValue(newForecast));
   }
   const handleAlpha = (e) => {
     setAlpha(parseFloat(e.target.value));
@@ -51,6 +71,8 @@ export default function PrediksiResult() {
   useEffect(() => {
     const forecast = sesForecast(data, 0.1)
     setResult(forecast);
+    setMad(madValue(forecast));
+    setMape(mapeValue(forecast));
   }, []);
 
   return (
@@ -66,14 +88,18 @@ export default function PrediksiResult() {
             <button onClick={handlePrediksi} class="btn btn-sm btn-success">Submit</button>
           </div>
         </div>
-        <div className="my-4">
-          <div className="w-50">
+        <div className="my-4 fore-wrap">
+          <div className="fore-table">
             <Tabel columns={columns} rows={result} />
           </div>
-          <div>
+          <div className="fore-form">
             <div className="mb-2">
-              <label class="form-label">MAPE</label>
-              <input type="number" value={alpha} name="mape" class="form-control form-control-sm" disabled />
+              <label class="form-label">{`Mean Absolute Deviation (MAD)`}</label>
+              <input type="number" value={mad} name="mape" class="form-control form-control-sm" disabled />
+            </div>
+            <div className="mb-2">
+              <label class="form-label">{`Mean Absolute Persentage Error (MAPE)`}</label>
+              <input type="number" value={mape} name="mape" class="form-control form-control-sm" disabled />
             </div>
           </div>
         </div>
